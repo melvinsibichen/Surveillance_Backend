c8180ef845bc1387d8e95ebc82038c4a4813cdec
melvinsibichen <melvinsibichen1020@gmail.com>
2023-04-26 19:35:20 +0530

    first commit

---
+++ src/test/java/com/nestdigital/Surveillance/SurveillanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.Surveillance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveillanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
